3689a87aa4e07cd1a9b78c6fd06885d4e40fc9ef
Joezeo <1252795700@qq.com>
2017-12-08 20:22:38 +0800

    python pro

---
+++ python-pro/io_prac.py
@@ -0,0 +1,25 @@
+forbideen = ('!', ',', '.', ';', '/', '?', ' ')
+
+
+def remove_forbideen(text):
+    text = text.lower()
+    for ch in text:
+        if ch in forbideen:
+            text = text.replace(ch, '')
+    return text
+
+
+def reverse(text):
+    return text[::-1]
+
+
+def is_palindrome(text):
+    return text == reverse(text)
+
+
+instr = input('Please input a string:')
+instr = remove_forbideen(instr)
+if is_palindrome(instr):
+    print('Yes, it is a palindrome')
+else:
+    print('No, it is not a palindrome')
