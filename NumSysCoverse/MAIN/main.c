501e4644c8717b8c8e3264295f29a6a817e55f3b
Joezeo <1252795700@qq.com>
2017-12-26 21:08:00 +0800

    libs refrence errors.

---
+++ NumSysCoverse/MAIN/main.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#pragma comment(lib, "F:/workspace/libs/data.struct.lib/DataStruct.lib")
+
+int main(int argc, char *argv[]){
+
+    NumSysConverse(158, 2);
+    return 0;
+
+}
+
+void
+NumSysConverse(const int _arg, const int _numsys){
+
+    int tmp = _arg;
+    int les = 0;
+    PSTACK psta = InitStack();
+
+    while (tmp != 0) {
+
+        les = tmp % _numsys;
+        Push(psta, &les, sizeof(int));
+        tmp /= _numsys;
+
+    }
+
+    while (!StackEmpty(psta)) {
+
+        Pop(psta, &tmp, sizeof(int));
+        printf("%d", tmp);
+
+    }
+
+    DestroyStack(psta);
+
+}
---
+++ NumSysCoverse/MAIN/main.h
@@ -0,0 +1,18 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdio.h>
+#include "F:/workspace/libs/data.struct.lib/dtastc.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void
+NumSysConverse(const int, const int);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MAIN_H__ //
