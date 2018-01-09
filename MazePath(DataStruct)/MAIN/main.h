13011fee6dee1523e244fe1c904b2931516f7da0
Joezeo <1252795700@qq.com>
2018-01-09 15:49:41 +0800

    add new project: maze path problem.

---
+++ MazePath(DataStruct)/MAIN/main.h
@@ -0,0 +1,17 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+#include <stdio.h>
+#include <conio.h>
+#include "F:/workspace/libs/data.struct.lib/dtastc.h"
+#include "../MAZE/maze.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MAIN_H__ //
---
+++ MazePath(DataStruct)/MAZE/maze.h
@@ -0,0 +1,14 @@
+#ifndef __MAZE_H__
+#define __MAZE_H__
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MAZE_H__ //
